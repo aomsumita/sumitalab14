52248c7a0b20c7ff06d2284bc415bfd5bf1c7543
aomsumita <sumita_n@ac.th.>
2024-01-11 00:32:58 +0700

    completed lab_13

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-const int N = 5;
+const int N = 4;
 
 void inputMatrix(double [][N]);
 
@@ -18,4 +18,38 @@ int main(){
 	cout << "Result is \n";
 	showMatrix(B);
 	return 0;
+}
+void inputMatrix(double A[N][N])
+{
+	for (int i = 0; i < N; i++)
+	{
+		cout << "Row " << i+1 << ": ";
+		for (int j = 0; j < N; j++)
+		{
+			cin >> A[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double A[N][N], bool B[N][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			B[i][j] = false;
+			if(i!=0 && j!=0 && i != N-1 && j != N-1){
+				if(A[i][j] >= A[i][j-1] && A[i][j] >= A[i][j+1] && A[i][j] >= A[i-1][j] && A[i][j] >= A[i+1][j]){
+					B[i][j] = true;
+				}
+			}
+		}
+	}
+}
+
+
+void showMatrix(const bool B[N][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			cout << B[i][j] << " ";
+		}
+		cout << "\n";
+	}
 }
